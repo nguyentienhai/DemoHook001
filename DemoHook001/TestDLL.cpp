557cbb85df7c8dc0d55f43edf560c37db9130cd1
Nguyễn Tiến Hải <Nguyen.TienHai@ymail.com>
2016-03-23 21:30:15 +0700

    Initialize project win32 DLL with first step, create DLL

---
+++ DemoHook001/TestDLL.cpp
@@ -0,0 +1,26 @@
+﻿#include <Windows.h>
+
+// vùng nhớ dùng chung, chứa biến handle của Hook
+#pragma data_seg("SHARED_DATA")
+HHOOK hGlobalHook = NULL;
+#pragma dataseg()
+
+// hàm lọc sự kiện nhấn phím
+__declspec(dllexport) LRESULT CALLBACK FillKeyboard(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	// nếu sự kiện là nhấn phím và mã phím là Enter
+	if(nCode == HC_ACTION && wParam == 13)
+	{
+		MessageBox(0, L"haint say hello :D", L"Hello", 0);
+		return 1;
+	}
+
+	// gọi Filter Function kế tiếp trong chuỗi các Filter Function
+	return CallNextHookEx(hGlobalHook, nCode, wParam, lParam);
+}
+
+// hàm ấn định biến hGlobalHook tại vùng nhớ dùng chung
+__declspec(dllexport) void SetGlobalHookHandle(HHOOK hHook)
+{
+	hGlobalHook = hHook;
+}
